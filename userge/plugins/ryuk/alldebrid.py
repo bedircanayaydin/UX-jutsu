d0f4671134da14c0300694a54c92389521f520f4
anonymousx97 <88324835+anonymousx97@users.noreply.github>
2023-06-11 20:39:41 +0530

    Post to TGH when magnets go over the limit. & clean up code.

--- userge/plugins/ryuk/alldebrid.py
+++ userge/plugins/ryuk/alldebrid.py
@@ -1,18 +1,22 @@
 # AllDebrid API plugin By Ryuk
 
 import os
+import asyncio
 
 import aiohttp
 from userge import Message, userge
+from userge.utils import post_to_telegraph as post_tgh
 
 # Your Alldbrid App token
 KEY = os.environ.get("DEBRID_TOKEN")
 
-# Optional Vars for advance users WebDav
-WEBDAV = os.environ.get("WEBDAV_URL")
-WEB_TORRENT = os.environ.get("WEB_TORRENT")
-WEB_LINK = os.environ.get("WEB_LINK")
-WEB_HISTORY = os.environ.get("WEB_HISTORY")
+
+TEMPLATE = """
+<b>Name</b>: <i>{name}</i>
+Status: <i>{status}</i>
+ID: {id}
+Size: {size}
+{uptobox}"""
 
 
 # Get response from api and return json or the error
@@ -40,23 +44,20 @@ async def get_json(endpoint: str, query: dict):
     },
 )
 async def debrid(message: Message):
-    if not ( link_ := message.filtered_input_str ):
-        return await message.reply("Give a magnet or link to unrestrict.",quote=True)
+    if not (link_ := message.filtered_input_str):
+        return await message.reply("Give a magnet or link to unrestrict.", quote=True)
     for i in link_.split():
         link = i
         if link.startswith("http"):
             if "-save" not in message.flags:
                 endpoint = "/link/unlock"
                 query = {"link": link}
-                d_link = WEB_HISTORY
             else:
                 endpoint = "/user/links/save"
                 query = {"links[]": link}
-                d_link = WEB_LINK
         else:
             endpoint = "/magnet/upload"
             query = {"magnets[]": link}
-            d_link = WEB_TORRENT
         unrestrict = await get_json(endpoint=endpoint, query=query)
         if not isinstance(unrestrict, dict) or "error" in unrestrict:
             return await message.reply(unrestrict, quote=True)
@@ -65,18 +66,13 @@ async def debrid(message: Message):
         else:
             if not link.startswith("http"):
                 data = unrestrict["data"]["magnets"][0]
-                name_ = data.get("name")
-                id_ = data.get("id")
-                size_ = round(int(data.get("size", 0)) / 1000000)
-                ready_ = data.get("ready")
             else:
                 data = unrestrict["data"]
-                name_ = data.get("filename")
-                id_ = data.get("id")
-                size_ = round(int(data.get("filesize", 0)) / 1000000)
-                ready_ = data.get("ready", "True")
-                d_link = d_link + name_ if d_link else ""
-            ret_str = f"""Name: **{name_}**\nID: `{id_}`\nSize: **{size_} mb**\nReady: __{ready_}__\nLink: {d_link.replace(" ","%20")}"""
+            name_ = data.get("filename", data.get("name",""))
+            id_ = data.get("id")
+            size_ = round(int(data.get("size", data.get("filesize",0))) / 1000000)
+            ready_ = data.get("ready", "True")
+            ret_str = f"""Name: **{name_}**\nID: `{id_}`\nSize: **{size_} mb**\nReady: __{ready_}__"""
             await message.reply(ret_str, quote=True)
 
 
@@ -85,67 +81,62 @@ async def debrid(message: Message):
     "torrents",
     about={
         "header": "Get singla magnet info or last 5 magnet info using id",
-        "flags": {
-            "-s": "-s {id} for status",
-            "-l": "limited number of results you want, defaults to 5",
-        },
+        "flags": {"-s": "-s {id} for status", "-l": "limited number of results you want, defaults to 1"},
         "usage": "{tr}torrents\n{tr}torrents -s 12345\n{tr}torrents -l 10",
     },
 )
 async def torrents(message: Message):
     endpoint = "/magnet/status"
     query = {}
+
     if "-s" in message.flags and "-l" in message.flags:
         return await message.reply("can't use two flags at once", quote=True)
+
     if "-s" in message.flags:
-        if not ( input_ := message.filtered_input_str ) :
+        if not (input_ := message.filtered_input_str):
             return await message.reply("ID required with -s flag", quote=True)
+
         query = {"id": input_}
+
     json = await get_json(endpoint=endpoint, query=query)
     if not isinstance(json, dict) or "error" in json:
         return await message.reply(json, quote=True)
+
     data = json["data"]["magnets"]
     if not isinstance(data, list):
-        status = data.get("status")
-        ret_val = f"""
-\n**Name**: __{data.get("filename")}__
-Status: __{status}__
-ID: ```{data.get("id")}```
-Size: """
-        if status == "Downloading":
-            ret_val += f"""__{round(int(data.get("downloaded",0))/1000000)}__/"""
-        ret_val += f"""__{round(int(data.get("size",0))/1000000)}__ mb"""
-        if (link := data.get("links")):
-                ret_val += "\n__UptoBox__: \n[ " + "\n".join([ f"""<a href={z.get("link","")}>{z.get("filename","")}</a>""" for z in link]) + " ]"
-        ret_val += f"\n\nSite: {WEB_TORRENT}" if WEB_TORRENT else ""
-    else:
-        ret_val = ""
-        limit = 5
-        if "-l" in message.flags:
-            limit = int(message.filtered_input_str)
-        for i in data[0:limit]:
-            status = i.get("status")
-            ret_val += f"""
-\nName: __{i.get("filename")}__
-Status: __{status}__
-ID: ```{i.get("id")}```
-Size: """
-            if status == "Downloading":
-                ret_val += f"""__{round(int(i.get("downloaded",0))/1000000)}__/"""
-            ret_val += f"""__{round(int(i.get("size",0))/1000000)}__ mb"""
-            if (link := i.get("links")):
-                ret_val += "\n__UptoBox__: \n[ " + "\n".join([ f"""<a href={z.get("link","")}>{z.get("filename","")}</a>""" for z in link]) + " ]"
-        ret_val += f"\n\nSite: {WEB_TORRENT}\n\nWebDav : {WEBDAV}" if WEBDAV and WEB_TORRENT else ""
-    await message.reply(ret_val, quote=True)
+        data = [data]
 
+    ret_str_list = []
+    limit = 1
+    if "-l" in message.flags:
+        limit = int(message.filtered_input_str)
+
+    for i in data[0:limit]:
+        status = i.get("status")
+        name = i.get("filename")
+        id = i.get("id")
+        downloaded = ""
+        uptobox = ""
+        if status == "Downloading":
+            downloaded = f"""<i>{round(int(i.get("downloaded",0))/1000000)}</i>/"""
+        size = f"""{downloaded}<i>{round(int(i.get("size",0))/1000000)}</i> mb"""
+        if link := i.get("links"):
+            uptobox = "<i>UptoBox</i>: \n[ " + "\n".join([f"""<a href={z.get("link","")}>{z.get("filename","")}</a>""" for z in link]) + " ]"
+        ret_str_list.append(ret_val := TEMPLATE.format(name=name, status=status, id=id, size=size, uptobox=uptobox))
+
+    ret_str = "<br>".join(ret_str_list)
+    if len(ret_str) < 4096:
+        await message.reply(ret_str, quote=True)
+    else:
+        await message.reply(post_tgh("Magnets", ret_str.replace("\n","<br>")), disable_web_page_preview=True, quote=True)
 
 
 # Delete a Magnet
-@userge.on_cmd("del_t",about={"header":"Delete the previously unrestricted Torrent","usage":"{tr}del_t 123456\n{tr}del_t 123456 78806"})
+@userge.on_cmd("del_t", about={"header": "Delete the previously unrestricted Torrent", "usage": "{tr}del_t 123456\n{tr}del_t 123456 78806"})
 async def delete_torrent(message: Message):
     endpoint = "/magnet/delete"
-    if not ( id := message.filtered_input_str ) :
+    if not (id := message.filtered_input_str):
         return await message.reply("Enter an ID to delete")
     for i in id.split():
-        json = await get_json(endpoint=endpoint, query={ "id": i })
+        json = await get_json(endpoint=endpoint, query={"id": i})
         await message.reply(str(json), quote=True)
